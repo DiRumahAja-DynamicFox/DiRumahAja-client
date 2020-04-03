dabe37dbe3728b1db2fd941f48abfcf701c436fb
thomasmartuaps <thomasmartuaps@gmail.com>
2020-04-03 11:33:14 +0700

    google sign in

--- main.js
+++ main.js
@@ -62,7 +62,7 @@ function showDashboard() {
     $('#landing-page').hide()
     $('#register-page').hide()
     $('#dashboard').show()
-    $('#favorites').hide()
+    generate()
 }
 
 function showRegister(event) {
@@ -79,8 +79,68 @@ function showFav() {
 
 function generate() {
     $('#favorites').hide()
-    $('#generated').show()
+    $('#generated').show(function() {
+        $.ajax({
+            method: 'GET',
+            url: `${localhost}/meme`
+        }).done(response => {
+            console.log(response.memedata)
+            $('#meme').empty()
+            $('#meme').append(` <img src="${response.memedata.url}"> `)
+        })
+        console.log('a')
+        console.log('b')
+        console.log('c')
+    })
     $('#generator').show()
+    console.log('d')
+    console.log('e')
+}
+
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    // console.log('Full Name: ' + profile.getName());
+    // console.log('Given Name: ' + profile.getGivenName());
+    // console.log('Family Name: ' + profile.getFamilyName());
+    // console.log("Image URL: " + profile.getImageUrl());
+    // console.log("Email: " + profile.getEmail());
+
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    // console.log("ID Token: " + id_token);
+
+    $.ajax({
+        method: "POST",
+        url: `${localhost}/users/googleSign`,
+        headers: {
+            token: id_token
+        }
+    }).done(function(response) {
+        console.log({
+            response: response,
+            msg: "Sent google token to server. Received server token."
+        })
+        localStorage.setItem("token", response.token)
+        showDashboard()
+    })
+    .fail(function(err) {
+        console.log(err, "<= It's an error on google signin")
+        err.responseJSON.forEach(el => {
+            $('#alert').append(`${el}<br>`)
+            $('#alert').fadeTo(2000, 500).slideUp(500, function(){
+                $("#alert").slideUp(500);
+                $('#alert').empty()
+            })
+        })
+    })
+}
+
+function logout(event) {
+    event.preventDefault()
+    localStorage.clear()
+    showLandingPage()
 }
 
 $(document).ready(function() {
@@ -89,6 +149,9 @@ $(document).ready(function() {
     let token = localStorage.getItem('token')
     if(!token) { 
         showLandingPage()
+    }
+    else {
+        showDashboard()
     }
 
 })
